ea9f9ae14ed0542ca811c5d3a472faa45ae863f5
favouritesam <favsamlol@gmail.com>
2023-01-18 23:38:34 +0100

    pythonAssignment

---
+++ Quiz.py
@@ -0,0 +1,43 @@
+#print("welcome to quiz Game")
+#print(question)
+#if the answer is correct
+#else print(the answer is correct)\
+
+
+
+print ("Welcome to quiz Game")
+
+print ("question One")
+ 
+answer = input(f"what is the name of Nigeria president")
+
+if answer == "muhammadu_Buhari":
+
+  print("Correct")
+
+else:
+  print("Correct answer is muhammadu_Buhari")
+
+
+print("question two")
+
+answer = input(f"What is the value of 2+2")
+
+if answer =="4":
+
+  print("correct")
+
+else:
+  print("correct answer is 4")
+
+
+print("question three")
+
+answer = input(f"what is the full meaning of cpu")
+
+if answer == "central processing unit":
+
+  print("correct")
+
+else:
+  print("correct answer is central processing unit")
